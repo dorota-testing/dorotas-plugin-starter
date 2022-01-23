38c30fc4824255591c4d58e2926f02c655fd9c03
Dorota <d.a.lewinska@gmail.com>
2022-01-23 21:32:13 +0000

    Second page options created.

---
+++ includes/dps-plugin-settings-page1.php
@@ -0,0 +1,319 @@
+<?php
+
+/** 
+ * custom option and settings for page 1
+ */
+function dps_settings_init()
+{
+	// register a new setting for "dps" page
+	register_setting('dps', 'dps_options', 'dps_validation_callback');
+
+	// register a new section in the "dps" page
+	add_settings_section(
+		'dps_first_section',
+		__('First section title.', 'dps'),
+		'dps_first_section_cb',
+		'dps'
+	);
+	// register a second section in the "dps" page
+	add_settings_section(
+		'dps_second_section',
+		__('This is second section.', 'dps'),
+		'dps_second_section_cb',
+		'dps'
+	);
+	// register a new field in the "dps_first_section" section, inside the "dps" page
+	add_settings_field(
+		'dps_dropdown', // as of WP 4.6 this value is used only internally
+		// use $args' label_for to populate the id inside the callback
+		__('Dropdown', 'dps'),
+		'dps_dropdown_cb',
+		'dps',
+		'dps_first_section',
+		[
+			'label_for' => 'dps_dropdown',
+			'class' => 'dps_row_wrap',
+			'dps_custom_data' => 'custom',
+		]
+	);
+	// register a new field in the "dps_first_section" section, inside the "dps" page
+	add_settings_field(
+		'dps_textfield', // as of WP 4.6 this value is used only internally
+		// use $args' label_for to populate the id inside the callback
+		__('Textfield', 'dps'),
+		'dps_textfield_cb',
+		'dps',
+		'dps_first_section',
+		[
+			'label_for' => 'dps_textfield',
+			'class' => 'dps_row_wrap',
+			'dps_custom_data' => 'lorem',
+		]
+	);
+	// register a new field in the "dps_first_section" section, inside the "dps" page
+	add_settings_field(
+		'dps_textarea', // as of WP 4.6 this value is used only internally
+		// use $args' label_for to populate the id inside the callback
+		__('Textarea', 'dps'),
+		'dps_textarea_cb',
+		'dps',
+		'dps_first_section',
+		[
+			'label_for' => 'dps_textarea',
+			'class' => 'dps_row_wrap',
+		]
+	);
+	// register a new field in the "dps_first_section" section, inside the "dps" page
+	add_settings_field(
+		'dps_image_id', // as of WP 4.6 this value is used only internally
+		// use $args' label_for to populate the id inside the callback
+		__('Image', 'dps'),
+		'dps_image_id_cb',
+		'dps',
+		'dps_second_section',
+		[
+			'label_for' => 'dps_image_id',
+			'class' => 'dps_row_wrap',
+		]
+	);
+	// register a new field in the "dps_first_section" section, inside the "dps" page
+	add_settings_field(
+		'dps_image2_id', // as of WP 4.6 this value is used only internally
+		// use $args' label_for to populate the id inside the callback
+		__('Image2', 'dps'),
+		'dps_image2_id_cb',
+		'dps',
+		'dps_second_section',
+		[
+			'label_for' => 'dps_image2_id',
+			'class' => 'dps_row_wrap',
+		]
+	);
+}
+
+/**
+ * register our dps_settings_init to the admin_init action hook
+ */
+add_action('admin_init', 'dps_settings_init');
+
+/**
+ * custom option and settings:
+ * callback functions
+ */
+function dps_validation_callback($input)
+{
+	// echo '<pre>'; print_r($input); echo '</pre>';
+	// die;
+	$success = true;
+	$error = '';
+	if ($input['dps_dropdown'] == '') {
+		//		die('error');	
+		$error = 'Please select a Dropdown.';
+		//		$input = get_option('dps_options');
+		$success = false;
+	}
+	if ($input['dps_textfield'] == '') {
+		//		die('error');
+		if ($error != '') {
+			$error .= '</br>';
+		}
+		$error .= 'Textfield is mandatory.';
+		//		$input = get_option('dps_options');
+		$success = false;
+	}
+
+	if ($success) {
+		add_settings_error('dps_messages', 'dps_message', __('Settings Saved lorem', 'dps'), 'updated');
+	} else {
+		// add one error for all		
+		add_settings_error('dps_messages', 'dps_message', __($error, 'dps'), 'error');
+	}
+
+	return $input;
+}
+
+// developers section cb
+
+// section callbacks can accept an $args parameter, which is an array.
+// $args have the following keys defined: title, id, callback.
+// the values are defined at the add_settings_section() function.
+function dps_first_section_cb($args)
+{
+?>
+	<p id="<?php echo esc_attr($args['id']); ?>"><?php esc_html_e('This is explanation text for the first section.', 'dps'); ?></p>
+<?php
+}
+function dps_second_section_cb($args)
+{
+?>
+	<p id="<?php echo esc_attr($args['id']); ?>"><?php esc_html_e('Consecterur adipiscing elit.', 'dps'); ?></p>
+<?php
+}
+
+// dropdown field cb
+
+// field callbacks can accept an $args parameter, which is an array.
+// $args is defined at the add_settings_field() function.
+// wordpress has magic interaction with the following keys: label_for, class.
+// the "label_for" key value is used for the "for" attribute of the <label>.
+// the "class" key value is used for the "class" attribute of the <tr> containing the field.
+// you can add custom key value pairs to be used inside your callbacks.
+function dps_dropdown_cb($args)
+{
+	// get the value of the setting we've registered with register_setting()
+	$options = get_option('dps_options');
+	//echo '<pre>'; print_r($options); echo '</pre>';
+	// output the field
+?>
+	<select id="<?php echo esc_attr($args['label_for']); ?>" data-custom="<?php echo esc_attr($args['dps_custom_data']); ?>" name="dps_options[<?php echo esc_attr($args['label_for']); ?>]">
+		<option value="" <?php echo isset($options[$args['label_for']]) ? (selected($options[$args['label_for']], '', false)) : (''); ?>>
+			<?php esc_html_e('Select...', 'dps'); ?>
+		</option>
+		<option value="red" <?php echo isset($options[$args['label_for']]) ? (selected($options[$args['label_for']], 'red', false)) : (''); ?>>
+			<?php esc_html_e('option one', 'dps'); ?>
+		</option>
+		<option value="blue" <?php echo isset($options[$args['label_for']]) ? (selected($options[$args['label_for']], 'blue', false)) : (''); ?>>
+			<?php esc_html_e('option two', 'dps'); ?>
+		</option>
+	</select>
+	<p class="description">
+		<?php esc_html_e('Some description goes here.', 'dps'); ?>
+	</p>
+	<p class="description">
+		<?php esc_html_e('Some more description follows.', 'dps'); ?>
+	</p>
+<?php
+}
+function dps_textfield_cb($args)
+{
+	// get the value of the setting we've registered with register_setting()
+	$options = get_option('dps_options');
+	//echo '<pre>'; print_r($options); echo '</pre>';
+	// output the field
+?>
+	<input type="text" id="<?php echo esc_attr($args['label_for']); ?>" data-lorem="<?php echo esc_attr($args['dps_custom_data']); ?>" name="dps_options[<?php echo esc_attr($args['label_for']); ?>]" value="<?= (isset($options[$args['label_for']]) ? $options[$args['label_for']] : '') ?>">
+
+	<p class="description">
+		<?php esc_html_e('Lorem ipsum dolor sit amet.', 'dps'); ?>
+	</p>
+<?php
+}
+function dps_textarea_cb($args)
+{
+	// get the value of the setting we've registered with register_setting()
+	$options = get_option('dps_options');
+	//echo '<pre>'; print_r($options); echo '</pre>';
+	// output the field
+?>
+	<textarea id="<?php echo esc_attr($args['label_for']); ?>" name="dps_options[<?php echo esc_attr($args['label_for']); ?>]"><?= (isset($options[$args['label_for']]) ? $options[$args['label_for']] : '') ?></textarea>
+
+	<p class="description">
+		<?php esc_html_e('Consectetur adipiscing elit.', 'dps'); ?>
+	</p>
+<?php
+}
+function dps_image_id_cb($args)
+{
+	// get the value of the setting we've registered with register_setting()
+	$options = get_option('dps_options');
+	$simple_popup_image_image_id = (isset($options['dps_image_id']) ? $options['dps_image_id'] : '');
+	//echo '<pre>'; print_r($options); echo '</pre>';
+	// output the field
+?>
+	<table>
+		<tr>
+			<td>
+				<table>
+					<tr>
+						<td>
+							<input class="image-id" type="hidden" name="dps_options[<?php echo esc_attr($args['label_for']); ?>]" value="<?= $simple_popup_image_image_id; ?>" />
+							<input type="button" class="button dps_upload-button" value="Select Image" />
+						</td>
+						<td>
+							<button class="button dps_deleteIcon" type="button" data-placeholder="<?= esc_url(plugins_url('img/placeholder.png', __FILE__)); ?>">Mark for Deletion</button>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<small>Image should be 540px wide and 400px high.</small>
+						</td>
+					</tr>
+				</table>
+			</td>
+			<td>
+				<img src="<?= ($simple_popup_image_image_id == '' ? esc_url(plugins_url('img/placeholder.png', __FILE__)) : wp_get_attachment_image_src($simple_popup_image_image_id)[0]) ?>" width="70" style="width: 70px;" />
+			</td>
+		</tr>
+
+	</table>
+<?php
+}
+function dps_image2_id_cb($args)
+{
+	// get the value of the setting we've registered with register_setting()
+	$options = get_option('dps_options');
+	$image_id = (isset($options['dps_image2_id']) ? $options['dps_image2_id'] : '');
+	//echo '<pre>'; print_r($options); echo '</pre>';
+	// output the field
+?>
+	<table>
+		<tr>
+			<td>
+				<table>
+					<tr>
+						<td>
+							<input class="image-id" type="hidden" name="dps_options[<?php echo esc_attr($args['label_for']); ?>]" value="<?= $image_id; ?>" />
+							<input type="button" class="button dps_upload-button" value="Select Image" />
+						</td>
+						<td>
+							<button class="button dps_deleteIcon" type="button" data-placeholder="<?= esc_url(plugins_url('img/placeholder.png', __FILE__)); ?>">Mark for Deletion</button>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<small>Image should be 540px wide and 400px high.</small>
+						</td>
+					</tr>
+				</table>
+			</td>
+			<td>
+				<img src="<?= ($image_id == '' ? esc_url(plugins_url('img/placeholder.png', __FILE__)) : wp_get_attachment_image_src($image_id)[0]) ?>" width='70' style='width: 70px;' />
+			</td>
+		</tr>
+
+	</table>
+<?php
+}
+
+
+/**
+ * top level menu: ???
+ * callback functions
+ */
+function dps_options_page_html()
+{
+	// check user capabilities
+	if (!current_user_can('manage_options')) {
+		return;
+	}
+
+	// show error/update messages
+	//  *****  NOTE - the line below needs to be uncomented if plugin has its own page in menu. If it is in submenu of settings or enything else, should be commented out. ****
+	settings_errors('dps_messages');
+?>
+	<div class="wrap">
+		<h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+		<form action="options.php" method="post">
+			<?php
+			// output security fields for the registered setting "dps"
+			settings_fields('dps');
+			// output setting sections and their fields
+			// (sections are registered for "dps", each field is registered to a specific section)
+			do_settings_sections('dps');
+			// output save settings button
+			submit_button('Save Settings');
+			?>
+		</form>
+	</div>
+<?php
+}
---
+++ includes/dps-plugin-settings-page2.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * custom option and settings for page 2
+ */
+function dps_settings_init_2()
+{
+	// register a new setting for "dps" page
+	register_setting('dps2', 'dps_options_page2', 'dps_validation_callback_2');
+
+	// register a new section in the "dp2s" page
+	add_settings_section(
+		'dps_first_section',
+		__('First section title on second page.', 'dps'),
+		'dps_first_section_cb_2',
+		'dps2'
+	);
+	// register a new field in the "dps_first_section" section, inside the "dps2" page
+	add_settings_field(
+		'dps_textfield_2', // as of WP 4.6 this value is used only internally
+		// use $args' label_for to populate the id inside the callback
+		__('Textfield', 'dps'),
+		'dps_textfield_cb_2',
+		'dps2',
+		'dps_first_section',
+		[
+			'label_for' => 'dps_textfield_2',
+			'class' => 'dps_row_wrap',
+			'dps_custom_data' => 'lorem-data',
+		]
+	);
+}
+/**
+ * register our dps_settings_init to the admin_init action hook
+ */
+add_action('admin_init', 'dps_settings_init_2');
+
+/**
+ * custom option and settings:
+ * callback functions
+ */
+function dps_validation_callback_2($input)
+{
+	// echo '<pre>'; print_r($input); echo '</pre>';
+	// die;
+	$success = true;
+	$error = '';
+	if ($input['dps_textfield_2'] == '') {
+		//		die('error');	
+		$error = 'Please fill in this field.';
+		//		$input = get_option('dps_options_page2');
+		$success = false;
+	}
+
+	if ($success) {
+		add_settings_error('dps_messages_2', 'dps_message', __('Settings Saved Second Page', 'dps'), 'updated');
+	} else {
+		// add one error for all		
+		add_settings_error('dps_messages_2', 'dps_message', __($error, 'dps'), 'error');
+	}
+
+	return $input;
+}
+// section callbacks can accept an $args parameter, which is an array.
+// $args have the following keys defined: title, id, callback.
+// the values are defined at the add_settings_section() function.
+function dps_first_section_cb_2($args)
+{
+?>
+	<p id="<?php echo esc_attr($args['id']); ?>"><?php esc_html_e('This is explanation text for the first section ON THE SECOND PAGE.', 'dps'); ?></p>
+<?php
+}
+
+function dps_options_page_html2()
+{
+	// check user capabilities
+	if (!current_user_can('manage_options')) {
+		return;
+	}
+
+	// show error/update messages
+	//  *****  NOTE - the line below needs to be uncomented if plugin has its own page in menu. If it is in submenu of settings or enything else, should be commented out. ****
+	settings_errors('dps_messages_2');
+?>
+	<div class="wrap">
+		<h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+		<form action="options.php" method="post">
+			<?php
+			// output security fields for the registered setting "dps2"
+			settings_fields('dps2');
+			// output setting sections and their fields
+			// (sections are registered for "dp2s", each field is registered to a specific section)
+			do_settings_sections('dps2');
+			// output save settings button
+			submit_button('Save Settings');
+			?>
+		</form>
+	</div>
+<?php
+}
+
+function dps_textfield_cb_2($args)
+{
+	// get the value of the setting we've registered with register_setting()
+	$options = get_option('dps_options_page2');
+	//echo '<pre>'; print_r($options_page2); echo '</pre>';
+	// output the field
+?>
+	<input type="text" id="<?php echo esc_attr($args['label_for']); ?>" data-lorem="<?php echo esc_attr($args['dps_custom_data']); ?>" name="dps_options_page2[<?php echo esc_attr($args['label_for']); ?>]" value="<?= (isset($options[$args['label_for']]) ? $options[$args['label_for']] : '') ?>">
+
+	<p class="description">
+		<?php esc_html_e('Lorem ipsum dolor sit amet.', 'dps'); ?>
+	</p>
+<?php
+}
